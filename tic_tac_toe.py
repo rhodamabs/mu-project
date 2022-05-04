f8a3db28ad60885d1159725b104ff0818d7fdda0
rhodamabs <mab21004@byui.edu>
2022-05-04 07:42:41 +0200

    Commit

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,32 +1,34 @@
 """
 Tic tac toe solution by Rhoda Mabundu
 """
-# Declaring a board container
-game_board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
 
+#printing the game-board for user to see
 
 def main():
-    player_1 = input('Enter your name: ')
-    player_2 = input('Enter your name: ')
-    game= print_game_board(game_board)
-    for num in range(9):
-        if num%2 == 0:
-            x = inpt(game_board)
+    game_board = create_board()
+    print_game_board(game_board)
+    for num in game_board:
+        #if num%2 == 0:
+            x = inp(game_board)
             game_board[x-1] = 'x'
+            print_game_board(game_board)
             if player_moves(game_board):
                 print(f'{player_1} wins!!!')
                 break
             else:
-                 o = inpt(game_board)
-            game_board[x-1] = 'o'
-            if player_moves(game_board):
-                print(f'{player_2} wins!!!')
-                break
-print('Game Over!!!!')        
-    
+                    o = inp(game_board)
+                    game_board[x-1] = 'o'
+                    print_game_board(game_board)
+                    if player_moves(game_board):
+                        print(f'{player_2} wins!!!')
+                        break
+            print('Game Over')
 
 
-#printing the game-board for user to see
+def create_board():
+    game_board = ['1', '2', '3', '4', '5', '6', '7', '8', '9',]
+    return game_board
+
 def print_game_board(game_board):
     print()
     print(f"{game_board[0]}|{game_board[1]}|{game_board[2]}")
@@ -35,9 +37,9 @@ def print_game_board(game_board):
     print('-+-+-')
     print(f"{game_board[6]}|{game_board[7]}|{game_board[8]}")
     print()
-    return print_game_board(game_board)
 
-#check the winner of the game
+
+
 def player_moves(game_board):
     player_1 = 'x'
     player_2 = 'o'
@@ -60,25 +62,15 @@ def player_moves(game_board):
     else:
         return False
 
-# User input
-def inpt(game_board):
-    x = int(input("Please choose a square (1-9): "))
-    if game_board[x - 1]!= '-':
-        print('Value exists choose a different value: ')
-        return inpt(game_board)
+def inp(game_board):
+    x = int(input('Enter number within the range of 1 to 9:'))
+    if game_board[x-1]!='-':     
+        print()
+        return inp(game_board)
     else:
-        return x
+         return x
 
-def quit(player_input):
-    while True:
-        player_input = (input('Enter a position 1 to 9 0r enter \"q\" to quit:'))
-        if quit(player_input): break
-    if player_input.lower() == 'q':
-       print('We are sad to see you go')
-       return True
-    else:
-        return False
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
+ 
